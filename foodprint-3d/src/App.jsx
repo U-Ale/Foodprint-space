c0d6fdd86f06216bbbb948534bbbaa207f3a945d
u-ale <alexandreprzrs@gmail.com>
2026-06-04 22:59:51 -0300

    first commit

---
+++ foodprint-3d/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>FoodPrint 3D</h1>
+      <p>
+        Sistema de Impressão 3D de Carne Vegetal
+      </p>
+    </div>
+  );
+}
+
+export default App;
