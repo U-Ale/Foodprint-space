c84be95a44f1cce78953749442650c7a8ec75e9b
u-ale <alexandreprzrs@gmail.com>
2026-06-06 18:21:34 -0300

    Pagina faq adicionada no dia 05/06 por volta das 4

--- foodprint-3d/src/App.jsx
+++ foodprint-3d/src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
 import Home from "./pages/Home";
+import FAQ from "./pages/FAQ";
 
 function App() {
-  return <Home />;
+  const [pagina, setPagina] = useState("home");
+
+  return (
+    <>
+      <nav>
+        <button onClick={() => setPagina("home")}>
+          Home
+        </button>
+
+        <button onClick={() => setPagina("faq")}>
+          FAQ
+        </button>
+      </nav>
+
+      {pagina === "home" && <Home />}
+      {pagina === "faq" && <FAQ />}
+    </>
+  );
 }
 
 export default App;
---
+++ foodprint-3d/src/pages/FAQ.jsx
@@ -0,0 +1,24 @@
+function FAQ() {
+  return (
+    <main>
+      <h1>Perguntas Frequentes</h1>
+
+      <h3>O que é a Source Code?</h3>
+      <p>
+        Uma startup focada em soluções tecnológicas para alimentação espacial.
+      </p>
+
+      <h3>Como funciona a impressora 3D?</h3>
+      <p>
+        Ela utiliza matérias-primas sustentáveis para produzir alimentos.
+      </p>
+
+      <h3>Quais ingredientes são utilizados?</h3>
+      <p>
+        Soja, ervilha, micoproteína e grão-de-bico.
+      </p>
+    </main>
+  );
+}
+
+export default FAQ;
