df8c0e0ed148b64ab76911296fa435b6d1cefada
u-ale <alexandreprzrs@gmail.com>
2026-06-07 18:07:56 -0300

    Estilo da pag integrantes alterada

--- foodprint-3d/src/pages/Integrantes.jsx
+++ foodprint-3d/src/pages/Integrantes.jsx
@@ -1,27 +1,27 @@
 function Integrantes() {
   return (
-    <main>
+    <main className="integrantes">
 
       <h1>Equipe Source Code</h1>
 
-      <section>
+      <section className="integrantes-grid">
 
-        <div>
+        <div className="integrante-card">
           <h2>Alexandre Prazeres</h2>
-          <p>RM:573059</p>
-          <p>Função:</p>
+          <p>RM: 573059</p>
+          <p>Função: Desenvolvedor</p>
         </div>
 
-        <div>
+        <div className="integrante-card">
           <h2>Matheus Nezio</h2>
-          <p>RM571702:</p>
-          <p>Função:</p>
+          <p>RM: 571702</p>
+          <p>Função: Desenvolvedor</p>
         </div>
 
-        <div>
+        <div className="integrante-card">
           <h2>Júlia Rodrigues</h2>
-          <p>RM571244:</p>
-          <p>Função:</p>
+          <p>RM: 571244</p>
+          <p>Função: Designer</p>
         </div>
 
       </section>
